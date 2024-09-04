3c4d802c59e92635e87b2214bc1d168c91d9b3a2
LanTuo <1264348626@qq.com>
2024-09-04 16:59:19 +0800

    feat: :sparkles: init

---
+++ src/layout/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import View from "../view"
+import styles from "./index.module.scss"
+
+function Layout() {
+  return (
+    <div className={styles.layout}>
+      <View />
+    </div>
+  )
+}
+
+export default Layout
---
+++ src/view/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from "./index.module.scss"
+function View() {
+  return (
+    <div className={styles.view}>
+      index
+    </div>
+  )
+}
+
+export default View
