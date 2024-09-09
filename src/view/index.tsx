1de92262eb8cc0562a5a3725ca3385950dd89356
LanTuo <1264348626@qq.com>
2024-09-09 14:55:34 +0800

    feat(test): :sparkles: test

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useState } from 'react';
 import styles from "./index.module.scss"
+import { Button } from "antd";
 function View() {
+  const [num, setNum] = useState<number>(0)
   return (
     <div className={styles.view}>
-      Hello Web3!
+      Hello Web3!{num}
+      <Button type="primary" onClick={() => { setNum(num + 1) }}>按钮</Button>
     </div>
   )
 }
